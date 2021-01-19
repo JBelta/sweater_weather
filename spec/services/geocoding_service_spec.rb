2a0e28856be729b223548fc98fe26c23232f9705
James Belta <james.belta@gmail.com>
2021-01-19 11:30:26 -0700

    geocoding service spec

---
+++ spec/services/geocoding_service_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe GeocodingService do
+  context 'instance methods' do
+    context '#location' do
+      it 'returns current weather of selected city, state' do
+        state_data = GeocodingService.location('Denver', 'CO')
+        hash = state_data[:results][0][:locations][0]
+
+        expect(hash[:adminArea5]).to eq('Denver')
+        expect(hash[:adminArea3]).to eq('CO')
+        expect(hash[:latLng][:lat]).to be_a(Float)
+        expect(hash[:latLng][:lat]).to eq(39.738453)
+        expect(hash[:latLng][:lng]).to be_a(Float)
+        expect(hash[:latLng][:lng]).to eq(-104.984853)
+      end
+    end
+  end
+end
+#city = City.new(state_data)
