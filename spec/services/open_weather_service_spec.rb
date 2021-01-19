a835bcd57002319e824f488c1445d64b1fb02c6b
James Belta <james.belta@gmail.com>
2021-01-18 20:13:48 -0700

    changed spec file name

---
+++ spec/services/open_weather_service_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe OpenWeatherService do
+  context 'instance methods' do
+    context '#current_weather' do
+      it 'returns current weather of selected city, state' do
+        denver = OpenWeatherService.current_weather('Denver')
+
+        expect(denver).to be_a(Hash)
+        expect(denver[:dt]).to be_an(Integer)
+        expect(denver[:sys][:sunrise]).to be_an(Integer)
+        expect(denver[:sys][:sundown]).to be_an(Integer)
+        expect(denver[:main][:temp]).to be_an(Integer)
+        expect(denver[:main][:feels_like]).to be_an(Integer)
+        expect(denver[:main][:humidity]).to be_an(Integer)
+        expect(denver[:visability]).to be_an(Integer)
+        expect(denver[:dt]).to be_an(Integer)
+        expect(denver[:weather][:description]).to be_a(String)
+      end
+    end
+
+
+
+  end
+end
