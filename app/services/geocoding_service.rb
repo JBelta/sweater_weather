9980d04caee77c1bc2b42e4f8b08d81c6b496029
James Belta <james.belta@gmail.com>
2021-01-19 11:31:23 -0700

    geo coding service recieving lat/ long. include city/ state

---
+++ app/services/geocoding_service.rb
@@ -0,0 +1,16 @@
+class GeocodingService
+
+  def self.location(city, state)
+    response = conn.get("/geocoding/v1/address?location=#{city},#{state}")
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    #json[:results][0][:locations][0]
+  end
+
+  def self.conn
+    Faraday.new(url: 'http://www.mapquestapi.com/') do |faraday|
+      faraday.params['key'] = ENV['GEOCODING_API_KEY']
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+end
