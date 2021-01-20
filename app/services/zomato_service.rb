6b90cc771a7d34f5740ecb74f2577b97b1372abe
James Belta <james.belta@gmail.com>
2021-01-20 10:08:07 -0700

    delete extra `https://`, added JSON.parse

--- app/services/zomato_service.rb
+++ app/services/zomato_service.rb
@@ -2,9 +2,10 @@ class ZomatoService
 
   def self.search_by_city(lat, lon)
     response = conn.get("/api/v2.1/cities?lat=#{lat}&lon=#{lon}")
+    JSON.parse(response.body, symbolize_names: true)
   end
   def self.conn
-    Faraday.new(url: 'https://https://developers.zomato.com') do |faraday|
+    Faraday.new(url: 'https://developers.zomato.com') do |faraday|
       faraday.headers['user-key'] = ENV['ZOMATO_KEY']
     end
   end
