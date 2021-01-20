e5e2b184e07542c9ac44efeb549e700f9e66a033
James Belta <james.belta@gmail.com>
2021-01-20 09:52:31 -0700

    zomato service

---
+++ app/services/zomato_service.rb
@@ -0,0 +1,11 @@
+class ZomatoService
+
+  def self.search_by_city(lat, lon)
+    response = conn.get("com/api/v2.1/cities?lat=#{lat}&lon=#{lon}")
+  end
+  def self.conn
+    Faraday.new(url: 'https://https://developers.zomato.com') do |faraday|
+      faraday.headers[user-key] = ENV['ZOMATO_KEY']
+    end
+  end
+end
