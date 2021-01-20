81f6034b5c4426779209c2a0eeb52cac2d9e82ad
James Belta <james.belta@gmail.com>
2021-01-20 10:08:36 -0700

    create var outcome for enver and pueblo

--- spec/services/zomato_service_spec.rb
+++ spec/services/zomato_service_spec.rb
@@ -4,8 +4,10 @@ describe ZomatoService do
   context 'It can find resturaunts with lat and long' do
     denver = GeocodingFacade.city_location('denver', 'co')
     pueblo = GeocodingFacade.city_location('pueblo', 'co')
+binding.pry
+    outcome_denver = ZomatoService.search_by_city(denver.lat, denver.lng)
 
-    outcome = ZomatoService.search_by_city(denver.lat, denver.lng)
+    outcome_pueblo = ZomatoService.search_by_city(pueblo.lat, pueblo.lng)
 
     binding.pry
   end
