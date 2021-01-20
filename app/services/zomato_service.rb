2460507e55bdc51f5f311cd597ea5e724d1cca96
James Belta <james.belta@gmail.com>
2021-01-20 10:30:52 -0700

    spacing

--- app/services/zomato_service.rb
+++ app/services/zomato_service.rb
@@ -4,6 +4,7 @@ class ZomatoService
     response = conn.get("/api/v2.1/search?q=#{food}lat=#{lat}&lon=#{lon}")
     JSON.parse(response.body, symbolize_names: true)
   end
+
   def self.conn
     Faraday.new(url: 'https://developers.zomato.com') do |faraday|
       faraday.headers['user-key'] = ENV['ZOMATO_KEY']
