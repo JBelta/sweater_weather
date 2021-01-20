6838c7ea193f48f5d4b1952e7e37a1d316f29180
James Belta <james.belta@gmail.com>
2021-01-20 03:22:29 -0700

    removed pry, unused code, weather_hourly calls openweatherservice.hourly

--- app/facade/open_weather_facade.rb
+++ app/facade/open_weather_facade.rb
@@ -1,13 +1,14 @@
 class OpenWeatherFacade
 
   def self.weather_now(lat, lon)
-    #binding.pry
     state_info = OpenWeatherService.current_weather(lat, lon)
     uv_info =
     OpenWeatherService.ultraviolet(lat, lon)
-
     data = state_info.merge(uv_info)
-    binding.pry
-    CityCurrentWeather.new(data)
+  end
+
+  def self.weather_hourly(lat, lon)
+    data = OpenWeatherService.hourly(lat, lon)
+    #eight_hours = data[:list].take(8)
   end
 end
