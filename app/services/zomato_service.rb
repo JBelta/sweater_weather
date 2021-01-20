9984e5586c03b39611cdcb5628684cbd01706e98
James Belta <james.belta@gmail.com>
2021-01-20 10:03:46 -0700

    delete `com` in end point

--- app/services/zomato_service.rb
+++ app/services/zomato_service.rb
@@ -1,7 +1,7 @@
 class ZomatoService
 
   def self.search_by_city(lat, lon)
-    response = conn.get("com/api/v2.1/cities?lat=#{lat}&lon=#{lon}")
+    response = conn.get("/api/v2.1/cities?lat=#{lat}&lon=#{lon}")
   end
   def self.conn
     Faraday.new(url: 'https://https://developers.zomato.com') do |faraday|
