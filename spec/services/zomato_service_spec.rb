5e44be7d448fee1de17ac82dfd6f9d029e5cfed5
James Belta <james.belta@gmail.com>
2021-01-20 10:15:34 -0700

    `it` block for expectations, followed service with [:location_suggestions].first

--- spec/services/zomato_service_spec.rb
+++ spec/services/zomato_service_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
 describe ZomatoService do
-  context 'It can find resturaunts with lat and long' do
-    denver = GeocodingFacade.city_location('denver', 'co')
-    pueblo = GeocodingFacade.city_location('pueblo', 'co')
+  context 'Connecting to Zomato api' do
+    it 'can find resteraunts with lattitude and longitude' do
+      denver = GeocodingFacade.city_location('denver', 'co')
+      pueblo = GeocodingFacade.city_location('pueblo', 'co')
+
+      outcome_denver = ZomatoService.search_by_city(denver.lat, denver.lng)[:location_suggestions].first
+      outcome_pueblo = ZomatoService.search_by_city(pueblo.lat, pueblo.lng)[:location_suggestions].first
 binding.pry
-    outcome_denver = ZomatoService.search_by_city(denver.lat, denver.lng)
-
-    outcome_pueblo = ZomatoService.search_by_city(pueblo.lat, pueblo.lng)
-
-    binding.pry
+    #denver
+      expect(outcome_denver).to be_a(Hash)
+      expect(outcome_denver)
+    end 
   end
 end
