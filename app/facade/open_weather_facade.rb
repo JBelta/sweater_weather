3015299e76768ce336362d2a6d3e8ea42595053e
James Belta <james.belta@gmail.com>
2021-01-20 03:45:09 -0700

    daily_weather method

--- app/facade/open_weather_facade.rb
+++ app/facade/open_weather_facade.rb
@@ -5,10 +5,16 @@ class OpenWeatherFacade
     uv_info =
     OpenWeatherService.ultraviolet(lat, lon)
     data = state_info.merge(uv_info)
+    CityCurrentWeather.new(data)
   end
 
   def self.weather_hourly(lat, lon)
     data = OpenWeatherService.hourly(lat, lon)
-    #eight_hours = data[:list].take(8)
+    CityHourlyWeather.new(data)
+  end
+
+  def self.daily_weather(lat, lon)
+    data = OpenWeatherService.daily(lat, lon)
+    CityDailyWeather.new(data)
   end
 end
